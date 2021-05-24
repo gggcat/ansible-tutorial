425f72cd5c053d420f3fe4a2477930c70a4b19e7
gggcat <61681628+gggcat@users.noreply.github.com>
2021-05-24 11:11:06 +0900

    ubuntu初期設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,19 @@
 mkdir -p ~/.ssh
 chown -R root:root ~/.ssh
 chmod -R 0700 ~/.ssh
-cp -ip /run/secrets/host_ssh_key ~/.ssh/id_rsa
+cp -ip /run/secrets/secret_key ~/.ssh/id_rsa
+cp -ip /run/secrets/public_key ~/.ssh/id_rsa.pub
 chmod -R 0600 ~/.ssh
 
 #
 # Install Ubuntsu package
 #
-apt-get update
-apt-get install -y software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt-get install -y ansible
+#apt-get update
+#apt-get install -y software-properties-common
+#apt-add-repository --yes --update ppa:ansible/ansible
+#apt-get install -y ansible
+apt-get install sshpass -y
+
+pip install ansible
 
 exec "$@"
